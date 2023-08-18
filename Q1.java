0fafd50d305e9cf39287fe7dc8d9bc3a13e2a7c4
Adithya R U <112653455+areyouadithya@users.noreply.github.com>
2023-08-18 07:56:04 +0000

    exec'd

--- Q1.java
+++ Q1.java
@@ -14,10 +14,10 @@ public class Q1 {
     diff = a-b;
     pro = a*b;
     quo = a/b;
-    System.out.println("The sum of "+a+"and "+b+"is "+sum);
-    System.out.println("The difference of "+a+"and "+b+"is "+diff);
-    System.out.println("The product of "+a+"and "+b+"is "+pro);
-    System.out.println("The quotient of "+a+"and "+b+"is "+quo);
+    System.out.println("The sum of "+a+" and "+b+" is "+sum);
+    System.out.println("The difference of "+a+" and "+b+" is "+diff);
+    System.out.println("The product of "+a+" and "+b+" is "+pro);
+    System.out.println("The quotient of "+a+" and "+b+" is "+quo);
     ip.close();
   }
 }
